9b251534e5224e47a8102dedf8c51bfe031e44d8
marsevilspirit <a226214089@qq.com>
2023-11-12 15:36:54 +0800

    avoid reading empty files

--- avoid_empty_files.c
+++ avoid_empty_files.c
@@ -11,3 +11,16 @@ int main(int argc,char *argv[])
         ch = getc(fp);
     }
 }
+//also could do this
+//#include<stdio.h>
+//int main(int argc,char *argv[])
+//{
+//    int ch;
+//    FILE *fp;
+//    fp = fopen(argv[1],"r");
+//    ch = getc(fp);
+//    while(ch = getc(fp)) != EOF)
+//    {
+//        putchar(ch);
+//    }
+//}
